6a206aa9489f453e991f993d13e280911e3e2f29
Chirag Patel <chiragp1982@yahoo.com>
2016-03-20 20:47:08 -0400

    updating restore

--- gitrestore.sh
+++ gitrestore.sh
@@ -1,3 +1,2 @@
 typeset file_to_delete=$1
-git checkout $(git rev-list -n 1 HEAD -- "${file_to_delete}")^ -- "${file_to_delete}"
-
+git checkout $(git rev-list -n 1 HEAD -- ${file_to_delete})^ -- ${file_to_delete}
